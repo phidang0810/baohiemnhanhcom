694718a8c2160728d432b4978be602f540d7f7c7
Dang diep <phidangmtv@gmail.com>
2021-10-30 12:29:57 +0700

    init

---
+++ administrator/lib/config.php
@@ -0,0 +1,11 @@
+<?php if(!defined('_lib')) die("Error");
+$config_url=$_SERVER["SERVER_NAME"];//."/congty/baohiem";
+
+
+$config['database']['servername'] = 'localhost';
+$config['database']['username'] = 'bhnhanhcom_l';
+$config['database']['password'] = 'apxt94Bk';
+$config['database']['database'] = 'bhnhanhcom_l';
+$config['database']['refix'] = 'table_';
+
+?>
---
+++ templates/layout/header.php
@@ -0,0 +1,62 @@
+﻿<?php
+$d->reset();
+$sql_cat="select* from table_tour_cat where hienthi=1 order by stt";
+$d->query($sql_cat);
+$result_cat=$d->result_array();
+
+$d->reset();
+$sql_cat="select* from table_news where hienthi=1 and id_item=2 order by stt";
+$d->query($sql_cat);
+$gioithieu=$d->result_array();
+
+$d->reset();
+$sql_cat="select* from table_news where hienthi=1 and id_item=3 order by stt";
+$d->query($sql_cat);
+$boithuong=$d->result_array();
+
+$d->reset();
+$sql="select * from #_photo where hienthi=1 and com='banner_top'";
+$d->query($sql);
+$item=$d->fetch_array();
+?>
+
+<div id="header" style=" no-repeat top left">
+        
+        <div id="banner" style="position:relative;height:118px">
+            <p style="margin-bottom:10px ; align:center "><embed height="119" width="960" type="application/x-shockwave-flash" allowscriptaccess="always" wmode="transparent" allowfullscreen="false" scale="noborder" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" quality="high" src="images/MIC.swf" title=" "/></p>
+
+
+         <div style="position:absolute;right:10px;bottom:10px"><a href="vi/index.html" id="vi"><img src="images/vi.png" alt="" /></a>&nbsp;<a id="en" href="en/index.html"><img src="images/en.png" alt="" /></a></div>
+  </div>
+        <div id="top-menu" style="position:relative">
+            <ul class="nav1" style="float:left">
+                <li><a href="<?=$lang?>/index.html" <?php if($_REQUEST['com']=='index'||$_REQUEST['com']=='') echo "class='active'";?>><?=_trangchu?></a></li>
+                <li><a href="<?=$lang?>/gioi-thieu-chung.html" <?php if($_REQUEST['com']=='gioi-thieu-chung') echo "class='active'";?>><?=_gioithieu?></a>
+                    <ul>
+                    <?php foreach($gioithieu as $item){ ?>
+                        <li><a href="<?=$lang?>/gioi-thieu/<?=$item['id']?>/<?=$item['tenkhongdau']?>.html"><?=$item['ten_'.$lang]?></a>
+                         </li><?php }?>
+                    </ul>
+                </li>
+                <li><a href="<?=$lang?>/san-pham.html" <?php if($_REQUEST['com']=='san-pham') echo "class='active'";?>><?=_sanpham?></a>
+                    <ul>
+                    <?php foreach($result_cat as $item){ ?>
+                        <li><a href="<?=$lang?>/sanpham/<?=$item['tenkhongdau']?>.html"><?=$item['ten_'.$lang]?></a>
+                         </li><?php }?>
+                </ul></li>
+                <li><a href="<?=$lang?>/boi-thuong-chung.html" <?php if($_REQUEST['com']=='boi-thuong-chung') echo "class='active'";?>><?=_boithuong?></a>
+                    <ul>
+                    <?php foreach($boithuong as $item){ ?>
+                        <li><a href="<?=$lang?>/boi-thuong/<?=$item['id']?>/<?=$item['tenkhongdau']?>.html"><?=$item['ten_'.$lang]?></a>
+                         </li><?php }?>
+                    </ul>
+                </li>  
+                <li><a href="<?=$lang?>/tin-tuc.html" <?php if($_REQUEST['com']=='tin-tuc') echo "class='active'";?>><?=_tintuc?></a></li>
+                     
+                <li><a href="<?=$lang?>/lien-he.html" <?php if($_REQUEST['com']=='lien-he') echo "class='active'";?>><?=_lienhe?></a></li>  
+                <li><a href="<?=$lang?>/co-hoi-viec-lam.html" <?php if($_REQUEST['com']=='co-hoi-viec-lam') echo "class='active'";?>><?=_cohoivieclam?></a> </li>
+
+            </ul>
+        </div>
+    <div class="clear"></div>
+    </div><!-- end header-->
