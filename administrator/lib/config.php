c8ec8fdcb9a028ea5a57fcf0ed26ef0880a34285
Dang diep <phidangmtv@gmail.com>
2021-10-30 12:39:33 +0700

    change db

--- administrator/lib/config.php
+++ administrator/lib/config.php
@@ -3,9 +3,9 @@ $config_url=$_SERVER["SERVER_NAME"];//."/congty/baohiem";
 
 
 $config['database']['servername'] = 'localhost';
-$config['database']['username'] = 'bhnhanhcom_l';
-$config['database']['password'] = 'apxt94Bk';
-$config['database']['database'] = 'bhnhanhcom_l';
+$config['database']['username'] = 'hs_web';
+$config['database']['password'] = 'hsDang3112de.@11';
+$config['database']['database'] = 'baohiemnhanhcom';
 $config['database']['refix'] = 'table_';
 
 ?>
